2499fb80431fd3d189be22aef9b86e0418a92d03
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-16 14:32:45 +0200

    add_task_7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+ const newStudents = students.find((students) => students.id === 2)
+  console.log(newStudents);
+  
+ 
+  
