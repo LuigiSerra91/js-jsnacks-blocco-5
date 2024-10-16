a237cab93adb0683a842a3628a18fab79aa488c5
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-16 13:50:11 +0200

    add_task3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,19 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const incrementNumbers = numbers.map(numbers => numbers + 1)
+
+console.log(incrementNumbers);
+
+
+let numbersTwo = []
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+     numbersTwo.push(number + 1)
+     
+     
+}
+
+console.log(numbersTwo);
